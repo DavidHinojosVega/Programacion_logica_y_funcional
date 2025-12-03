2f04c08e6685cfb5cc22d965a1a6090a3e867d3a
DavidHinojosVega <david.hinojos.vega0110@gmail.com>
2025-12-03 16:14:30 -0600

    Update practica2.pl

--- logica/practica2.pl
+++ logica/practica2.pl
@@ -1,3 +1,7 @@
+% ==========================================
+% BASE DE HECHOS
+% ==========================================
+
 desendiente(hominoidea, hominidea).
 desendiente(hominoidea, hylobatidae).
 desendiente(hominidea, homininae).
@@ -12,10 +16,8 @@ desendiente(pan,bonobo).
 desendiente(pan,chinpanzee).
 desendiente(hominini,homo).
 desendiente(homo,human).
-desendiente(hominoidea, hylobatidae).
 desendiente(hylobatidae,hylobates).
 desendiente(hylobates,gibbon).
-
 
 categoria(superfamily,hominoidea).
 categoria(family,hominidea).
@@ -31,47 +33,153 @@ categoria(genus,pongo).
 categoria(genus,hylobates).
 
 
+% ==========================================
+% 1- Es el gorila descendiente de hominini?
+% ==========================================
+
+% desendiente(hominini,gorilla).
+% false.
 
 
+% ==========================================
+% 2- ¿Es el Homo hermano del gorila?
+% ==========================================
+
+% ?- desendiente(X,homo),desendiente(Y,gorilla).
+% X = hominini,
+% Y = gorilini.
 
 
+% ==========================================
+% 3- ¿Son el Homo y el Gorilla de la misma subfamilia?
+% ==========================================
 
-%1- Es el gorila descendiente de hominini?
+misma_subfamilia(X,Y):-desendiente(A,X),desendiente(B,A),categoria(subfamily,B),desendiente(C,Y),desendiente(D,C),categoria(subfamily,D),B=D.
 
-%desendiente(hominini,gorilla).
-%false.
-
-%2- ¿Es el Homo hermano del gorila?
-
-%?- desendiente(X,homo),desendiente(Y,gorilla).
-%X = hominini,
-%Y = gorilini.
-
-%3- ¿Son el Homo y el Gorilla de la misma subfamilia?
-%subfamilia(X,Y):- desendiente(A,X),desendiente(B,A),desendiente(C,Y),desendietne(D,C), categoria(Z,D),categoria(H,B)
+% ?- misma_subfamilia(homo,gorilla).
+% true.
 
 
+% ==========================================
+% 4- ¿Tiene Hominini la misma familia que Pongo?
+% ==========================================
 
-%4- ¿Tiene Hominini la misma familia que Pongo?
+misma_familia(X,Y):-desendiente(A,X),categoria(family,A),desendiente(B,Y),categoria(family,B),A=B.
 
-%5- ¿Quiénes son los descendientes directos de una tribu?
+% ?- misma_familia(hominini,pongo).
+% true.
 
-%6- ¿Cuál es la superfamilia a la que pertenece Hylobates?
 
-%7- ¿Qué géneros comparten la misma subfamilia con Pan?
+% ==========================================
+% 5- ¿Quiénes son los descendientes directos de una tribu?
+% ==========================================
 
-%8- ¿Cuál es el ancestro común más cercano entre Homo y Gorilla?
+% ?- desendiente(hominini,X).
+% X = pan ;
+% X = homo.
 
-%9- ¿Es el orangután un homínido?
 
-%10- ¿Cuáles son todas las especies que pertenecen a la familia Hominidae?
+% ==========================================
+% 6- ¿Cuál es la superfamilia a la que pertenece Hylobates?
+% ==========================================
 
-%11- ¿Qué géneros son hermanos taxonómicos de Homo?
+superfamilia(X,SF):-desendiente(SF,X),categoria(superfamily,SF).
 
-%12 -¿Comparten Homo y Hylobates la misma familia?
+% ?- superfamilia(hylobates,SF).
+% SF = hominoidea.
 
-%13- ¿Cuáles son los descendientes directos de la subfamilia Homininae?
 
-%14- ¿Qué géneros están bajo la familia Hominidae?
+% ==========================================
+% 7- ¿Qué géneros comparten la misma subfamilia con Pan?
+% ==========================================
 
-%15- ¿Cuál es la ruta taxonómica completa desde Homo hasta la superfamilia?
+mismos_generos_subfamilia_pan(G):-categoria(genus,G),desendiente(S,G),desendiente(S,pan),categoria(subfamily,S),G\=pan.
+
+% ?- mismos_generos_subfamilia_pan(G).
+% G = gorilla ;
+% G = homo.
+
+
+% ==========================================
+% 8- ¿Cuál es el ancestro común más cercano entre Homo y Gorilla?
+% ==========================================
+
+ancestro_comun(X,Y,A):-desendiente(A,X),desendiente(A,Y).
+
+% ?- ancestro_comun(homo,gorilla,A).
+% A = homininae ;
+% A = hominoidea.
+
+
+% ==========================================
+% 9- ¿Es el orangután un homínido?
+% ==========================================
+
+es_hominido(X):-desendiente(F,X),categoria(family,F).
+
+% ?- es_hominido(orangutan).
+% true.
+
+
+% ==========================================
+% 10- ¿Qué especies pertenecen a la familia Hominidae?
+% ==========================================
+
+% En tu base Hominidae = hominidea
+
+% ?- desendiente(hominidea,X),desendiente(X,Y),desendiente(Y,Z).
+% X = homininae, Y = hominini, Z = pan ;
+% X = homininae, Y = hominini, Z = homo ;
+% X = homininae, Y = gorilini, Z = gorilla ;
+% X = poginae, Y = pongo, Z = orangutan.
+
+
+% ==========================================
+% 11- ¿Qué géneros son hermanos taxonómicos de Homo?
+% ==========================================
+
+hermanos_homo(G):-categoria(genus,G),desendiente(S,G),desendiente(S,homo),G\=homo.
+
+% ?- hermanos_homo(G).
+% G = pan.
+
+
+% ==========================================
+% 12- ¿Comparten Homo y Hylobates la misma familia?
+% ==========================================
+
+misma_familia2(X,Y):-desendiente(A,X),categoria(family,A),desendiente(B,Y),categoria(family,B),A=B.
+
+% ?- misma_familia2(homo,hylobates).
+% false.
+
+
+% ==========================================
+% 13- ¿Cuáles son los descendientes directos de la subfamilia Homininae?
+% ==========================================
+
+% ?- desendiente(homininae,X).
+% X = gorilini ;
+% X = hominini.
+
+
+% ==========================================
+% 14- ¿Qué géneros están bajo la familia Hominidae?
+% ==========================================
+
+% hominidae = hominidea en tu base
+
+% ?- desendiente(hominidea,X),categoria(genus,X).
+% X = homo ;
+% X = pan ;
+% X = gorilla.
+
+
+% ==========================================
+% 15- ¿Cuál es la ruta taxonómica completa desde Homo hasta la superfamilia?
+% ==========================================
+
+ruta_homo([X,A,B,C]):-desendiente(A,X),desendiente(B,A),desendiente(C,B),categoria(superfamily,C).
+
+% ?- ruta_homo(R).
+% R = [homo, hominini, homininae, hominoidea].
